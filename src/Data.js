8c79de88c95bb064f4635f573897be28fc79d946
Mehul-kapoor-21 <mehulkapoor308@gmail.com>
2023-05-31 17:05:15 +0530

    initial commit

---
+++ src/Data.js
@@ -0,0 +1,86 @@
+const shopItemsData = [
+    {
+      id: "121",
+      name: "Shirt",
+      price: 45,
+      desc: "Shirts",
+      img: "images/img1.jfif",
+    },
+    {
+      id: "122",
+      name: "Shirt",
+      price: 35,
+      desc: "Shirts",
+      img: "images/img2.jfif",
+    },
+    {
+      id: "123",
+      name: "Shirt",
+      price: 55,
+      desc: "Shirts",
+      img: "images/img3.jfif",
+    },
+    {
+      id: "124",
+      name: "Shirt",
+      price: 65,
+      desc: "Shirts",
+      img: "images/img4.jfif"
+    },
+    {
+      id: "125",
+      name: "Suit",
+      price: 63,
+      desc: "Suit",
+      img: "images/img5.jfif"
+    },
+    {
+      id: "126",
+      name: "Suit",
+      price: 55,
+      desc: "Suit",
+      img: "images/img6.jfif"
+    },
+    {
+      id: "127",
+      name: "Suit",
+      price: 60,
+      desc: "Suit",
+      img: "images/img7.jfif"
+    },
+    {
+      id: "128",
+      name: "Suit",
+      price: 70,
+      desc: "Suit",
+      img: "images/img8.jfif"
+    },
+    {
+      id: "129",
+      name: "Kurta",
+      price: 30,
+      desc: "Kurta",
+      img: "images/img9.jfif"
+    },
+    {
+      id: "130",
+      name: "Kurta",
+      price: 33,
+      desc: "Kurta",
+      img: "images/img10.jfif"
+    },
+    {
+      id: "131",
+      name: "Kurta",
+      price: 25,
+      desc: "Kurta",
+      img: "images/img11.jfif"
+    },
+    {
+      id: "132",
+      name: "Kurta",
+      price: 35,
+      desc: "Kurta",
+      img: "images/img12.jfif"
+    }
+  ];
